3a253625e2ab4984641f1c55ed6688fdb8cd64cc
ketan <ketanrtd1@gmail.com>
2022-09-27 09:12:55 +0530

    items management admin side done

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,15 +11,28 @@ module.exports = {
         let currDate = year + "-" + month + "-" + day;
         console.log(currDate)
 
+    if(req.params.date){
+        date = req.params.date;
+        Item.find({_id: date }, function (err, doc) {
+            console.log(doc)  // Ohk the doc is an array, that's why i was getting errors
+            if(typeof doc ==='undefined' || doc.length == 0){
+            res.render("itemsOnDate", {itemsToday: []})
+            } else {
+                res.render("itemsOnDate", {itemsToday: doc[0]["items"], date: date})
+            }
+            // res.send(doc[0]["items"])
+        });
+    } else {
     Item.find({_id: currDate }, function (err, doc) {
         console.log(doc)  // Ohk the doc is an array, that's why i was getting errors
         if(doc.length == 0){
         res.render("itemsOnDate", {itemsToday: []})
         } else {
-            res.render("itemsOnDate", {itemsToday: doc[0]["items"]})
+            res.render("itemsOnDate", {itemsToday: doc[0]["items"], date: currDate})
         }
         // res.send(doc[0]["items"])
     });
+    }
     },
 
     getAdminItemsOnDate : (req, res)=>{
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // /admin/...
 router
 .get("/items", adminControllers.getItemsOnDate)
-.get("/items/:date", adminControllers.getAdminItemsOnDate)
+.get("/items/:date", adminControllers.getItemsOnDate)
 // .post("/items/:date", adminControllers.getAdminItemsOnDate), there is already function for that put that in here or use it from there 
 .get("/",adminControllers.getAdminPanel)
 
