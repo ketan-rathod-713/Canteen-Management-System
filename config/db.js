61a8f6c9a16c7cd29759af6c3a4a18da06c2165f
ketan <ketanrtd1@gmail.com>
2022-10-05 23:11:01 +0530

    added database

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   dev : {
-  database: "mongodb://localhost:27017/CanteenItems", // for local development
+  database: "mongodb+srv://admin-ketan:Ketan@cluster0.odeen.mongodb.net/CanteenItems", // for local development
   },
   
   prod: {
